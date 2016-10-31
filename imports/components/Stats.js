2741289e8be5a7dba906640f1f0adb8c4b478ce5
Travis Wood <traviswood@Traviss-MacBook-Air.local>
2016-10-31 15:11:01 -0700

    Make review rating an average instead of first elem of array

--- imports/components/Stats.js
+++ imports/components/Stats.js
@@ -10,20 +10,24 @@ class Stats extends React.Component {
   render() {
     //Gets full user data from database
     var user = Meteor.users.find({'_id': Meteor.user()._id}).fetch()[0];
+    //Compute avg rating for user
+    var ratings = user.reviews || [];
+    var count = 0;
+    for (var i = 0; i < ratings.length; i++) {
+      count += ratings[i];
+    }
+    var avgRating = count / ratings.length;
+
     return (
       <div className='stats'>
         <h3> Usage Stats </h3>
         <div className='avgReviews'>
           <h5> Your current rating average is </h5>
-          <h3> {user.reviews[0]} </h3>
+          <h3> {avgRating} </h3>
         </div>
         <div className='avgCallLength'>
           <h5> Your average chat has been... </h5>
           <h3> (some value) minutes long </h3>
-        </div>
-        <div className='calledLocations'>
-          <h5> You have talked to people from </h5>
-          <li> some list of locations </li>
         </div>
       </div>
     );

906feb8bb987a6a60a3d30b7f30941ad81f67156
bianca <bianca.subion@gmail.com>
2016-11-02 16:13:59 -0700

    merge conflicts solve

--- imports/components/Dashboard.js
+++ imports/components/Dashboard.js
@@ -205,103 +205,136 @@ class Dashboard extends React.Component {
   }
 
 
-  flipCard() {
-     console.log(document.querySelector(".flip-container").className);
-      if (document.querySelector(".flip-container").className.includes('flip-activate')) {
-        document.querySelector(".flip-container").className = "flip-container"
+  flipCardTranslate() {
+     console.log(document.querySelector("#translate-container").className);
+      if (document.querySelector("#translate-container").className.includes('flip-activate')) {
+        document.querySelector("#translate-container").className = "flip-container"
       } else {
-        document.querySelector(".flip-container").className += ' flip-activate'
+        document.querySelector("#translate-container").className += ' flip-activate'
       }
    }
 
-  render() {
-    return (
-      <div className='dashboard'>
-        <div className='top'>
-          <div className='video-box'>
-            {!this.state.callDone &&
-              <div className='video-wrapper'>
-                {!this.state.callLoading && !this.state.currentCall &&
-                  <Welcome numMatches={this.props.onlineUsers.length}/>
-                }
-                {this.state.callLoading &&
-                  <Waiting />
-                }
-                <video ref='myVideo' id='myVideo' muted='true' autoPlay='true' 
-                  className={this.state.callLoading ? 'hidden' : null}></video>
-                <video ref='theirVideo' id='theirVideo' muted='true' autoPlay='true'
-                  className={this.state.callLoading ? 'hidden' : null}></video>
-              </div>
-            }
+   flipCardProfile() {
+     var classArray = document.querySelector("#profile-container").classList;
+     var isFlippedForward = classArray.contains('flip-forward');
+     var isFlippedBackward = classArray.contains('flip-backward');
 
-            {!this.state.currentCall && this.state.callDone &&
-              <Review 
-                partner={this.state.partner}
-                clearPartner={this.clearPartner.bind(this)}
-              />
-            }
-          </div>
-          <div className='profile'>
-            <div className='sign-out'>
-              <AccountsUIWrapper />
-            </div>
-            <UserProfile user={this.props.user}/>
-          </div>
-        </div>
-        <div className='bottom'>
-          <div className='text-box'>
-          {
-            this.state.partner &&
+     if (isFlippedForward || isFlippedBackward) {
+       document.querySelector("#profile-container").classList.toggle('flip-forward');
+       document.querySelector("#profile-container").classList.toggle('flip-backward');
+     } else {
+       document.querySelector("#profile-container").classList.toggle('flip-forward');
+     }
+   }
 
-            <div className="clock-suggestion-wrapper">
-              <div className="flip-container">
-                <div className="flipper">
-                  <div className="front">
-                    <Clock setCurrentLanguage={this.setCurrentLanguage.bind(this)} partner={this.state.partner} callDone={this.state.callDone} handleSpeechActive={this.flipCard.bind(this)}/>
-                  </div>
-                  <div className="back">
-                    <SpeechToTextBox handleSpeechActive={this.flipCard.bind(this)} currentLanguage={this.state.currentLanguage} oppositeLanguage={this.state.oppositeLanguage}/>
-                  </div>
-                </div>
-              </div>
-              <GoogleTranslate translated={this.state.translated} handleTextChange={this.handleTextChange.bind(this)} handleTextSubmit={this.handleTextSubmit.bind(this)}/>
-            </div>
-          }
-            {
-              !this.state.partner &&
-              <div className='waiting-for-match'>Waiting for match...</div>
-            }
-          </div>
-          <div className='new-chat'>
-            <div className='selected-language'>
-              Selected Languages
-            </div>
-            <div className='language'>
-              {
-               `${this.props.user.profile.language} / 
-                ${this.props.user.profile.learning}`
-              }
-            </div>
-            <div className='button-wrapper'>
-              {!this.props.onlineUsers[0] &&
-                <button>Waiting</button>
-              }
-              {this.props.onlineUsers[0] && !this.state.currentCall &&
-                <button onClick={this.startChat.bind(this, this.props.onlineUsers, this.props.peer)}>
-                  Start Chat
-                </button>
-              }
-              {this.state.currentCall &&
-                <button onClick={this.endChat.bind(this)}>
-                  End Chat
-                </button>
-              }
-            </div>
-          </div>
-        </div>
-      </div>
-    );
-  }
+
+   render() {
+     var context = this;
+     return (
+       <div className='dashboard'>
+         <div className='top'>
+           <div className='video-box'>
+             {!this.state.callDone &&
+               <div className='video-wrapper'>
+                 {!this.state.callLoading && !this.state.currentCall &&
+                   <Welcome numMatches={this.props.onlineUsers.length}/>
+                 }
+                 {this.state.callLoading &&
+                   <Waiting />
+                 }
+                 <video ref='myVideo' id='myVideo' muted='true' autoPlay='true' 
+                   className={this.state.callLoading ? 'hidden' : null}></video>
+                 <video ref='theirVideo' id='theirVideo' muted='true' autoPlay='true'
+                   className={this.state.callLoading ? 'hidden' : null}></video>
+               </div>
+             }
+
+             {!this.state.currentCall && this.state.callDone &&
+               <Review 
+                 partner={this.state.partner}
+                 clearPartner={this.clearPartner.bind(this)}
+               />
+             }
+           </div>
+
+
+             <div className="flip-container" id="profile-container">
+               <div className="flipper">
+                 <div className="front">
+                   <div className='profile'>
+                     <div className='sign-out'>
+                       <img src='http://res.cloudinary.com/small-change/image/upload/v1478038849/BitmapIcon_lkjnj3.png'
+                        className='menu-icon' onClick={function(){context.flipCardProfile()}}/>
+                       <AccountsUIWrapper />
+                     </div>
+                     <UserProfile user={this.props.user}/>
+                   </div>
+                 </div>
+                 <div className="back">
+                   <div className='profile'>
+                   <img src='http://res.cloudinary.com/small-change/image/upload/v1478038849/BitmapIcon_lkjnj3.png'
+                    className='menu-icon' onClick={function(){context.flipCardProfile()}}/>
+                     <p> hello world </p>
+                   </div>
+                 </div>
+               </div>
+             </div>
+
+         </div>
+         <div className='bottom'>
+           <div className='text-box'>
+           {
+             this.state.partner &&
+
+             <div className="clock-suggestion-wrapper">
+               <div className="flip-container bea" id="translate-container">
+                 <div className="flipper">
+                   <div className="front">
+                     <Clock setCurrentLanguage={this.setCurrentLanguage.bind(this)} partner={this.state.partner} callDone={this.state.callDone} handleSpeechActive={this.flipCardTranslate.bind(this)}/>
+                   </div>
+                   <div className="back">
+                     <SpeechToTextBox handleSpeechActive={this.flipCardTranslate.bind(this)} currentLanguage={this.state.currentLanguage} oppositeLanguage={this.state.oppositeLanguage}/>
+                   </div>
+                 </div>
+               </div>
+               <GoogleTranslate translated={this.state.translated} handleTextChange={this.handleTextChange.bind(this)} handleTextSubmit={this.handleTextSubmit.bind(this)}/>
+             </div>
+           }
+             {
+               !this.state.partner &&
+               <div className='waiting-for-match'>Waiting for match...</div>
+             }
+           </div>
+           <div className='new-chat'>
+             <div className='selected-language'>
+               Selected Languages
+             </div>
+             <div className='language'>
+               {
+                `${this.props.user.profile.language} / 
+                 ${this.props.user.profile.learning}`
+               }
+             </div>
+             <div className='button-wrapper'>
+               {!this.props.onlineUsers[0] &&
+                 <button>Waiting</button>
+               }
+               {this.props.onlineUsers[0] && !this.state.currentCall &&
+                 <button onClick={this.startChat.bind(this, this.props.onlineUsers, this.props.peer)}>
+                   Start Chat
+                 </button>
+               }
+               {this.state.currentCall &&
+                 <button onClick={this.endChat.bind(this)}>
+                   End Chat
+                 </button>
+               }
+             </div>
+           </div>
+         </div>
+       </div>
+     );
+   }
 }
 
 export default Dashboard;
